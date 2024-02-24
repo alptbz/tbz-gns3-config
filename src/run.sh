cd3a603614e1d5b994448e3948e79081333fe4d2
Philipp Albrecht <philipp@uisa.ch>
2024-02-24 02:12:31 +0100

    flow improvements

--- src/run.sh
+++ src/run.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+INSTALLED_FILE="/opt/cloudinitinstall/tbz-gns3-config.installed"
+
+if [ -f "$INSTALLED_FILE" ]; then
+    echo "already installed - the file $INSTALLED_FILE exists - exit"
+    exit 1
+fi 
+
+
 CURR_DIR=$(dirname $0)
 
 bash $CURRDIR/local-image-download-install.sh
@@ -7,4 +15,4 @@ bash $CURRDIR/local-image-download-install.sh
 systemctl disable /opt/cloudinitinstall/tbz-gns3-config/src/tbz-gns3-config.service
 logger "Disabled tbz-gns3-config.service"
 
-touch /opt/cloudinitinstall/tbz-gns3-config.installed
+touch $INSTALLED_FILE
