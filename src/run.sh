dec75d21fc91ec2941205c49891e3d563ed5f373
Philipp Albrecht <philipp@uisa.ch>
2024-02-24 02:09:12 +0100

    refactoring

--- src/run.sh
+++ src/run.sh
@@ -5,6 +5,6 @@ CURR_DIR=$(dirname $0)
 bash $CURRDIR/local-image-download-install.sh
 
 systemctl disable /opt/cloudinitinstall/tbz-gns3-config/src/tbz-gns3-config.service
-
 logger "Disabled tbz-gns3-config.service"
+
 touch /opt/cloudinitinstall/tbz-gns3-config.installed
